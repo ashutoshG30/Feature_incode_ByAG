3dc6dd72289ecb1163409418d12ca6aaec898221
ashutoshG30 <121443584+ashutoshG30@users.noreply.github.com>
2023-03-21 21:15:32 +0530

    Add files via upload

---
+++ BulkyBook.Data/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BulkyBook.Models;
+
+namespace BulkyBook.DataAccess
+{
+    public class ApplicationDbContext:DbContext
+    {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<CoverType> CoverTypes { get; set; }
+    }
+}
